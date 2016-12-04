a3bbf1a7bc8683718830d9333eba90b2ebfa14d8
HelderSepu <heldersepu@gmail.com>
2016-12-03 22:53:15 -0500

    Start fuction to GetCar

--- OffLeaseOnly/Controllers/ScrapeController.cs
+++ OffLeaseOnly/Controllers/ScrapeController.cs
@@ -10,28 +10,39 @@ namespace OffLeaseOnly.Controllers
 {
     public class ScrapeController : ApiController
     {
+        const string URL = "http://www.offleaseonly.com/used-cars/type_used/page_{0}/";
         public Dictionary<string, int> Get()
         {
             var cars = new Dictionary<string, int>();
-
             var web = new HtmlWeb();
-            var doc = web.Load("http://www.offleaseonly.com/used-cars/type_used/page_20/");
-            var vehicles = doc.DocumentNode.DescendantsWithClass("div", "vehicle-listing");
-            foreach (var veh in vehicles)
+            for (int i = 1; i < 10; i++)
             {
-                string vin = null;
-                var vinObj = veh.DescendantsWithClass("div", "second-half").FirstOrDefault();
-                if (vinObj != null)
+                var doc = web.Load(string.Format(URL, i));
+                var vehicles = doc.DocumentNode.DescendantsWithClass("div", "vehicle-listing");
+                foreach (var veh in vehicles)
                 {
-                    var vinContainer = vinObj.DescendantsWithClass("div", "container").Skip(2).FirstOrDefault();
-                    if (vinContainer != null)
-                        vin = vinContainer.InnerText;
+                    var car = GetCar(veh);
+                    if (car.vin != null)
+                        cars.Add(car.vin, 0);
                 }
-                if (vin != null)
-                    cars.Add(vin, 0);
             }
-
             return cars;
+        }
+
+        private Car GetCar(HtmlNode vehNode)
+        {
+            var car = new Car();
+            var vinObj = vehNode.DescendantsWithClass("div", "second-half").FirstOrDefault();
+            if (vinObj != null)
+            {
+                var vinContainer = vinObj.DescendantsWithClass("div", "container").Skip(2).FirstOrDefault();
+                if (vinContainer != null)
+                {
+                    car.vin = vinContainer.DescendantsWithClass("span", "spec-data").FirstOrDefault().InnerText;
+
+                }
+            }
+            return car;
         }
     }
 }
