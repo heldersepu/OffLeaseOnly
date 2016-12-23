d5553ac17a7b491ebdb528b338b3476afe9f2176
HelderSepu <heldersepu@gmail.com>
2016-12-22 20:25:46 -0500

    better sample to the GET: api/Cars

--- OffLeaseOnly/Controllers/CarsController.cs
+++ OffLeaseOnly/Controllers/CarsController.cs
@@ -20,6 +20,7 @@ namespace OffLeaseOnly.Controllers
         }
 
         // GET: api/Cars?take=10&skip=0&query=
+        // GET: api/cars?take=100&skip=0&query=price<19000+AND+mileage<12000+AND+cleanCarFax=1+AND+eng="4"+AND+make!="Kia"
         public IEnumerable<Car> Get(int take, int skip, string query)
         {
             return Cars.Data.Where(query).Skip(skip).Take(take);
