51cdb356d0bed2f355237a3d237182aad86e789f
HelderSepu <HelderSepu@gmail.com>
2017-05-09 11:03:02 -0400

    Abstract part of Cars to a Base class

---
+++ OffLeaseOnly/DAL/Base.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Caching;
+
+namespace OffLeaseOnly
+{
+    public abstract class Base<T>
+    {
+        public static string BaseDir
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+
+        public static string MemKey
+        {
+            get
+            {
+                return typeof(T).ToString() + "Data";
+            }
+        }
+
+        public static List<T> Data
+        {
+            get
+            {
+                var data = new List<T>();
+                var memCache = MemoryCache.Default.Get(MemKey);
+                if (memCache == null)
+                    return ReadJson;
+                else
+                    return (List<T>)memCache;
+            }
+        }
+
+        public static List<T> ReadJson
+        {
+            get
+            {
+                var data = new List<T>();
+                var allfiles = Directory.GetFiles(BaseDir, "cars?.json");
+                foreach (var path in allfiles)
+                    data.AddRange(JsonConvert.DeserializeObject<List<T>>(File.ReadAllText(path)));
+                UpdateMemCache(data);
+                return data;
+            }
+        }
+
+        public static void UpdateMemCache(List<T> data)
+        {
+            var policy = new CacheItemPolicy { SlidingExpiration = TimeSpan.FromHours(8) };
+            MemoryCache.Default.Add(MemKey, data, policy);
+        }
+
+    }
+}
--- OffLeaseOnly/DAL/Cars.cs
+++ OffLeaseOnly/DAL/Cars.cs
@@ -1,52 +1,13 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Dynamic;
-using System.IO;
 using System.Linq;
-using System.Runtime.Caching;
 
 namespace OffLeaseOnly
 {
-    public static class Cars
+    public class Cars: Base<Car>
     {
-        private static string CAR_DATA = "CarData";
-        public static string BaseDir { get { return AppDomain.CurrentDomain.BaseDirectory; } }
-
         public static string JsonPath = BaseDir + @"\cars{0}.json";
         public static string CsvPath = BaseDir + @"\cars.csv";
-
-        public static List<Car> Data
-        {
-            get
-            {
-                var data = new List<Car>();
-                var memCache = MemoryCache.Default.Get(CAR_DATA);
-                if (memCache == null)
-                    return ReadJson;
-                else
-                    return (List<Car>)memCache;
-            }
-        }
-
-        public static List<Car> ReadJson
-        {
-            get
-            {
-                var data = new List<Car>();
-                var allfiles = Directory.GetFiles(BaseDir, "cars?.json");
-                foreach (var path in allfiles)
-                    data.AddRange(JsonConvert.DeserializeObject<List<Car>>(File.ReadAllText(path)));
-                UpdateMemCache(data);
-                return data;
-            }
-        }
-
-        public static void UpdateMemCache(List<Car> data)
-        {
-            var policy = new CacheItemPolicy { SlidingExpiration = TimeSpan.FromHours(8) };
-            MemoryCache.Default.Add(CAR_DATA, data, policy);
-        }
 
         public static dynamic Statistics(List<Car> cars)
         {
