5e65efb35c8def84df3023496e17cdd9899e86da
HelderSepu <HelderSepu@gmail.com>
2017-05-09 15:03:42 -0400

    Add method to Save the prices

---
+++ OffLeaseOnly/Extensions/PricesExtension.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OffLeaseOnly
+{
+    public static class PricesExtension
+    {
+        public static void Save(this List<PriceHistory> prices)
+        {
+            Prices.UpdateMemCache(prices);
+
+            using (var file = File.CreateText(Prices.JsonPath))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(file, prices);
+            }
+        }
+    }
+}
