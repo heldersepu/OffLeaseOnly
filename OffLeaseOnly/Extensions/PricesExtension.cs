d78cc3cf2504e4b8f43f4f7f90b87fa5642079ee
HelderSepu <HelderSepu@gmail.com>
2017-05-10 09:43:11 -0400

    Add Statistics to PricesExtension

--- OffLeaseOnly/Extensions/PricesExtension.cs
+++ OffLeaseOnly/Extensions/PricesExtension.cs
@@ -1,11 +1,18 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace OffLeaseOnly
 {
     public static class PricesExtension
     {
+        public static Dictionary<int, int> Statistics(this List<PriceHistory> prices)
+        {
+            var p = prices.Select(x => x.price.OrderBy(y => y.Value).FirstOrDefault());
+            return p.GroupBy(x => x.Key).ToDict(); ;
+        }
+
         public static void Save(this List<PriceHistory> prices)
         {
             Prices.UpdateMemCache(prices);
