f3a9c249eccdd44c628e3b00368941cc7c8aa266
HelderSepu <HelderSepu@gmail.com>
2018-02-15 12:10:31 -0500

    Remove WebActivatorEx

--- OffLeaseOnly/App_Start/SwaggerConfig.cs
+++ OffLeaseOnly/App_Start/SwaggerConfig.cs
@@ -4,7 +4,7 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Http.Routing.Constraints;
 
-using WebActivatorEx;
+using System.Web;
 using OffLeaseOnly;
 using Swagger.Net.Application;
 using Swagger.Net;
