793fa9c4c10efa6211a8b630c49fd17e70032dc2
Helder Sepulveda <heldersepu@gmail.com>
2020-06-29 18:05:44 -0400

    Add controller to retrieve prices

---
+++ OffLeaseOnly/Controllers/PricesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Web.Http;
+
+namespace OffLeaseOnly.Controllers
+{
+    [RoutePrefix("api/Prices")]
+    public class PricesController : ApiController
+    {
+        // GET: api/Prices/1235
+        [Route("{vin}")]
+        public IEnumerable<PriceHistory> GetByVin(string vin)
+        {
+            return Prices.Data.Where(x => x.vin.Contains(vin));
+        }
+    }
+}
